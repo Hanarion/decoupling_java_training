8c57717e8cb2a3afe5e654eb14a1d2ae94cee589
Hanarion <landryjuge@gmail.com>
2022-02-11 15:59:06 +0100

    grfed

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -2,7 +2,7 @@ package fr.lernejo.logger;
 
 public class LoggerFactory {
     public static Logger getLogger(String name) {
-        Logger log = new ContextualLogger(name, new FileLogger("output.txt"));
+        Logger log = new CompositeLogger(new ConsoleLogger(), new FileLogger("output.txt"));
         return log;
     }
 }
